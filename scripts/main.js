d7bbf4f92e602bffb119f3b7cce2467000dd4428
Brianna Presgraves <bmp89@drexel.edu>
2023-03-21 11:45:52 -0400

    Update Signs

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,25 @@
 if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
-    astrological_sign = 'Capricorn'
+    astrological_sign = 'Capricorn'//Belgian Sheepdog
   } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
-    astrological_sign = 'Sagittarius'
+    astrological_sign = 'Sagittarius'//Brittany Spaniel
   } else if ((month === 10 && day >= 24) || (month === 11 && day <= 21)) {
-    astrological_sign = 'Scorpio'
+    astrological_sign = 'Scorpio'//American Bloodhound
   } else if ((month === 9 && day >= 23) || (month === 10 && day <= 23)) {
-    astrological_sign = 'Libra'
+    astrological_sign = 'Libra'//Samoyed
   } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
-    astrological_sign = 'Virgo'
+    astrological_sign = 'Virgo'//Sheltie
   } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
-    astrological_sign = 'Leo'
+    astrological_sign = 'Leo'//Golden Retriever
   } else if ((month === 6 && day >= 22) || (month === 7 && day <= 22)) {
-    astrological_sign = 'Cancer'
+    astrological_sign = 'Cancer'//Border Collie
   } else if ((month === 5 && day >= 21) || (month === 6 && day <= 21)) {
-    astrological_sign = 'Gemini'
+    astrological_sign = 'Gemini'//Jack Russell Terrier
   } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
-    astrological_sign = 'Taurus'
+    astrological_sign = 'Taurus'//Italian Greyhound
   } else if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
-    astrological_sign = 'Aries'
+    astrological_sign = 'Aries'//German Shepherd
   } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
-    astrological_sign = 'Pisces'
+    astrological_sign = 'Pisces'//Aussie
   } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
-    astrological_sign = 'Aquarius'
+    astrological_sign = 'Aquarius'//Pitbull Mix
   }
